83eba0b0aa5710b7821ec36980669414bcc167f8
VatriaWally <s2200111@student.unklab.ac.id>
2024-11-04 07:57:02 +0700

    memperbaiki halaman login

---
+++ src/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer style={{ textAlign: 'center', padding: '10px', background: '#f1f1f1' }}>
+            <p>&copy; 2024 Your Project Name. All rights reserved.</p>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/logo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Logo = () => {
+    return (
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+            <img src="path/to/your/logo.png" alt="Logo" style={{ height: '50px' }} />
+            <h1 style={{ marginLeft: '10px' }}>Your Brand</h1>
+        </div>
+    );
+};
+
+export default Logo;
